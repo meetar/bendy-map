b4667b74143218c7dfd47935849a1885e049bac1
Peter Richardson <m33tar@gmail.com>
2015-09-26 14:40:19 -0400

    first

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # bendy-map
-A bit Inception, and bit Here&amp;There.
+
+A map made with [Tangram](http://github.com/tangrams/tangram).
+
+Live demo: http://meetar.github.io/bendy-map
+
+### To run locally:
+
+Start a web server in the repo's directory:
+
+    python -m SimpleHTTPServer 8000
+    
+If that doesn't work, try:
+
+    python -m http.server 8000
+    
+Then navigate to: [http://localhost:8000](http://localhost:8000)
---
+++ main.js
@@ -0,0 +1,64 @@
+/*jslint browser: true*/
+/*global Tangram, gui */
+
+map = (function () {
+    'use strict';
+
+    var locations = {
+        'Oakland': [37.8044, -122.2708, 15],
+        'New York': [40.7185, -74.0013, 14.5],
+        'Seattle': [47.5937, -122.3215, 15]
+    };
+
+    var map_start_location = locations['New York'];
+
+    /*** URL parsing ***/
+
+    // leaflet-style URL hash pattern:
+    // #[zoom],[lat],[lng]
+    var url_hash = window.location.hash.slice(1, window.location.hash.length).split('/');
+
+    if (url_hash.length == 3) {
+        map_start_location = [url_hash[1],url_hash[2], url_hash[0]];
+        // convert from strings
+        map_start_location = map_start_location.map(Number);
+    }
+
+    /*** Map ***/
+
+    var map = L.map('map',
+        {"keyboardZoomOffset" : .05}
+    );
+
+    var layer = Tangram.leafletLayer({
+        scene: 'scene.yaml',
+        numWorkers: 2,
+        attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>',
+        unloadInvisibleTiles: false,
+        updateWhenIdle: false,
+        zoomControl: false,
+        minZoom: 14.5,
+        maxZoom: 14.5
+    });
+
+    window.layer = layer;
+    var scene = layer.scene;
+    window.scene = scene;
+
+    // setView expects format ([lat, long], zoom)
+    map.setView(map_start_location.slice(0, 3), map_start_location[2]);
+
+    var hash = new L.Hash(map);
+
+    /***** Render loop *****/
+
+    window.addEventListener('load', function () {
+        // Scene initialized
+        layer.on('init', function() {
+        });
+        layer.addTo(map);
+    });
+
+    return map;
+
+}());
